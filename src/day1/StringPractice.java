22a39438abf93397c59f77c9524e50b95dab0d71
metinkara <metinkara@gmail.com>
2020-02-29 15:25:06 -0500

    modified StringPractice class

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -4,6 +4,7 @@ public class StringPractice {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-
+        String str = "Java is fun!";
+        System.out.println(str);
     }
 }
